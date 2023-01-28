e72ce68bb95c4b1400c6a7d702e328a22208f2b0
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-01-28 14:58:25 +0100

    Podstawowa wersja aplikacji Spring Boot

---
+++ src/main/java/com/example/wsbfinalproject2022/WsbFinalProject2022Application.java
@@ -0,0 +1,13 @@
+package com.example.wsbfinalproject2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbFinalProject2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbFinalProject2022Application.class, args);
+	}
+
+}
