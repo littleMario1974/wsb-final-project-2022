9416d3aab570bd1c1ba9d036171781978858f8cf
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-02-12 17:53:17 +0100

    Utworzenie interfejsu PersonRepository #10

---
+++ src/main/java/com/example/wsbfinalproject2022/person/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.example.wsbfinalproject2022.person;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.Optional;
+
+public interface PersonRepository extends CrudRepository <Person, Long> {
+
+    Optional<Person> findByUsername(String username);
+
+    Optional<Person> findByUsernameAndEnabled(String username, Boolean enabled);
+
+    Iterable<Person> findAllByUsernameOrPassword(String s1, String s2);
+
+    Iterable<Person> findAllByDateCreatedAfter(Date d);
+}
