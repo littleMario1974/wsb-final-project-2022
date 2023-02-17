d86181e52cc30783c714c455c8d6896203526818
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-02-17 17:29:49 +0100

    dodanie adnotacji RestController i  EnableGlobalMethodSecurity - contact #27

--- src/main/java/com/example/wsbfinalproject2022/WsbFinalProject2022Application.java
+++ src/main/java/com/example/wsbfinalproject2022/WsbFinalProject2022Application.java
@@ -2,8 +2,12 @@ package com.example.wsbfinalproject2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WsbFinalProject2022Application {
 
 	public static void main(String[] args) {
