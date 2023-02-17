b1ce056adaeffa0829920e1897feb437cb2a3337
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-02-17 16:38:32 +0100

    usunięcie WebSecurityConfigurerAdapter - deprecated rozwinięcie klasy SecurityConfig

--- src/main/java/com/example/wsbfinalproject2022/config/SecurityConfig.java
+++ src/main/java/com/example/wsbfinalproject2022/config/SecurityConfig.java
@@ -1,29 +1,61 @@
 package com.example.wsbfinalproject2022.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
+    private final CustomUserDetailsService customUserDetailsService;
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/css/**");
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
                 .antMatchers("/contact").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .logout().permitAll();
+                .formLogin().loginPage("/login").permitAll();
+
+        return httpSecurity.build();
     }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
+
+        provider.setUserDetailsService(customUserDetailsService);
+
+        return provider;
+    }
+
+
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("wsb")
+                .password(encoder.encode("secret"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }*/
+
 }
