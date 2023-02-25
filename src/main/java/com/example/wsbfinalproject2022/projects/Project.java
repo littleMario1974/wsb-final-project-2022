1b9ee349f23891d9c8cc59671d89be0a4c9a5ab9
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-02-25 18:46:09 +0100

    Rozbudowa klasy Project

--- src/main/java/com/example/wsbfinalproject2022/projects/Project.java
+++ src/main/java/com/example/wsbfinalproject2022/projects/Project.java
@@ -1,9 +1,10 @@
 package com.example.wsbfinalproject2022.projects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.wsbfinalproject2022.person.Person;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 @Entity
@@ -12,12 +13,23 @@ public class Project {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String code;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
+    @Column(nullable = false)
     private Boolean enabled = true;
+
+    @Column(nullable = false)
+    private Date dateCreated;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable =false)
+    private Person creator;
 
 }
