22e754ac31da5fe60c6d25a63026506ef3e4e510
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-04-23 22:31:35 +0200

    IssueRepository Query

--- src/main/java/com/example/wsbfinalproject2022/issues/IssueRepository.java
+++ src/main/java/com/example/wsbfinalproject2022/issues/IssueRepository.java
@@ -1,7 +1,21 @@
 package com.example.wsbfinalproject2022.issues;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface IssueRepository extends JpaRepository<Issue, Long> {
+import java.util.List;
+
+@Repository
+public interface IssueRepository extends JpaRepository<Issue, Long>, JpaSpecificationExecutor<Issue> {
+    @Query(value = "select * from issue where enabled = :enabled",
+            nativeQuery = true)
+    List<Issue> findByEnabledNative(@Param("enabled") Boolean enabled);
+
+    List<Issue> findAllByEnabled(Boolean enabled);
+
 }
+
 
