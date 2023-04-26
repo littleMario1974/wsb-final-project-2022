b1ca3f0a9766d30119595b7e6b7e167403a7049f
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-04-27 00:12:08 +0200

    Utworzenie klasy IssueFilter

---
+++ src/main/java/com/example/wsbfinalproject2022/issues/IssueFilter.java
@@ -0,0 +1,56 @@
+package com.example.wsbfinalproject2022.issues;
+
+import com.example.wsbfinalproject2022.person.Person;
+import com.example.wsbfinalproject2022.projects.Project;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueFilter {
+
+
+    private Project project;
+
+    private Person assignee;
+
+    private Status status;
+
+    private String globalSearch;
+
+    public Specification<Issue> buildQuery() {
+        return Specification.anyOf(
+                ilike("name", globalSearch),
+                ilike("description", globalSearch),
+                ilike("code", globalSearch)
+
+
+        ).and(
+                Specification.allOf(
+                        equalTo("project", project),
+                        equalTo("enabled", true),
+                        equalTo("assignee", assignee),
+                        equalTo("status", status)
+                )
+        );
+    }
+
+    private Specification<Issue> equalTo(String property, Object value) {
+        if (value == null) {
+            return Specification.where(null);
+        }
+
+        return (root, query, builder) -> builder.equal(root.get(property), value);
+    }
+
+    private Specification<Issue> ilike(String property, String value) {
+        if (value == null) {
+            return Specification.where(null);
+        }
+
+        return (root, query, builder) -> builder.like(builder.lower(root.get(property)), "%" + value.toLowerCase() + "%");
+    }
+}
