2993d0a21d907897189735041ff814ee73d8987b
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-05-14 17:33:02 +0200

    sortowanie

--- src/main/java/com/example/wsbfinalproject2022/issues/IssueFilter.java
+++ src/main/java/com/example/wsbfinalproject2022/issues/IssueFilter.java
@@ -5,6 +5,7 @@ import com.example.wsbfinalproject2022.projects.Project;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 
 @Data
@@ -55,12 +56,34 @@ public class IssueFilter {
         return (root, query, builder) -> builder.like(builder.lower(root.get(property)), "%" + value.toLowerCase() + "%");
     }
 
-    public String toQueryString(Integer page) {
-        return "page=" + page +
+    public String toQueryString(Integer page, Sort sort) {
+        return "sort=" + toSortString(sort) +
+                "&page=" + page +
                 (project != null ? "&project=" + project.getId() : "") +
                 (assignee != null ? "&assignee=" + assignee.getId() : "") +
                 (status != null ? "&status=" + status : "") +
                 (globalSearch != null ? "&globalSearch=" + globalSearch : "");
     }
 
+    public String toSortString(Sort sort) {
+        Sort.Order order = sort.getOrderFor("name");
+        String sortString = "";
+        if (order != null) {
+            sortString += "name," + order.getDirection();
+        }
+
+        return sortString;
+    }
+
+    public Sort findNextSorting(Sort currentSorting) {
+        Sort.Direction currentDirection = currentSorting.getOrderFor("name") != null ? currentSorting.getOrderFor("name").getDirection() : null;
+
+        if (currentDirection == null) {
+            return Sort.by("name").ascending();
+        } else if (currentDirection.isAscending()) {
+            return Sort.by("name").descending();
+        } else {
+            return Sort.unsorted();
+        }
+    }
 }
