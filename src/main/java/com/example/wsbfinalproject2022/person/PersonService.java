cbe3d6a339559b622afa89047b46a44da5ce44b5
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-06-11 20:19:13 +0200

    haszowanie hasła tylko wtedy gdy zostało zmienione

--- src/main/java/com/example/wsbfinalproject2022/person/PersonService.java
+++ src/main/java/com/example/wsbfinalproject2022/person/PersonService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,10 +37,32 @@ public class PersonService {
     }*/
 
     protected void savePerson(Person person) {
-        String hashedPassword = bCryptPasswordEncoder.encode(person.getPassword());
-        person.setPassword(hashedPassword);
+        if (person.getId() != null) {
+            // Pobierz istniejącą osobę z bazy danych
+            Optional<Person> existingPersonOptional = personRepository.findById(person.getId());
+            if (existingPersonOptional.isPresent()) {
+                Person existingPerson = existingPersonOptional.get();
+                String existingPassword = existingPerson.getPassword();
+
+                // Sprawdź, czy hasło zostało zmienione
+                if (!existingPassword.equals(person.getPassword())) {
+                    // Hasło zostało zmienione, zahaszuj je ponownie
+                    String hashedPassword = bCryptPasswordEncoder.encode(person.getPassword());
+                    person.setPassword(hashedPassword);
+                } else {
+                    // Hasło nie zostało zmienione, nie trzeba go ponownie zapisywać
+                    person.setPassword(existingPassword);
+                }
+            }
+        } else {
+            // Nowy użytkownik, zahaszuj hasło
+            String hashedPassword = bCryptPasswordEncoder.encode(person.getPassword());
+            person.setPassword(hashedPassword);
+        }
+
         person.setEnabled(true);
         person.setDateCreated(Date.from(Instant.now()));
         personRepository.save(person);
     }
+
 }
