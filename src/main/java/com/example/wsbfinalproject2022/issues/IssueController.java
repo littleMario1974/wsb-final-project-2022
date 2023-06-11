3ef75d2f8e8b843445b71f8281be0a7fa3162ae0
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-06-11 19:11:41 +0200

    walidacja issue save

--- src/main/java/com/example/wsbfinalproject2022/issues/IssueController.java
+++ src/main/java/com/example/wsbfinalproject2022/issues/IssueController.java
@@ -2,14 +2,16 @@ package com.example.wsbfinalproject2022.issues;
 
 import com.example.wsbfinalproject2022.authorities.AuthorityRepository;
 import com.example.wsbfinalproject2022.person.PersonRepository;
-import com.example.wsbfinalproject2022.projects.ProjectFilter;
 import com.example.wsbfinalproject2022.projects.ProjectRepository;
 import com.example.wsbfinalproject2022.projects.ProjectService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,7 +38,8 @@ public class IssueController {
     }
 
     @GetMapping
-    ModelAndView index(@ModelAttribute IssueFilter filter, ProjectFilter projectFilter, Pageable pageable) {
+    @Secured("ROLE_MANAGE_PROJECT")
+    ModelAndView index(@ModelAttribute IssueFilter filter, Pageable pageable) {
 
         Page<Issue> issues = issueService.findAll(filter, pageable);
 
@@ -51,38 +54,70 @@ public class IssueController {
     }
 
     @GetMapping("/create")
+    @Secured("ROLE_MANAGE_PROJECT")
     ModelAndView create() {
         ModelAndView modelAndView = new ModelAndView("issues/create");
         Issue issue = new Issue();
         modelAndView.addObject("issue", issue);
-        modelAndView.addObject("projects", projectRepository.findAll(Sort.by(Sort.Order.by("code")).ascending())); // w widoku Zadań wyświetli wszystkie projekty
+        modelAndView.addObject("projects", projectRepository.findAllByEnabled(true)); // w widoku Zadań wyświetli wszystkie projekty
         modelAndView.addObject("person", personRepository.findAll(Sort.by(Sort.Order.by("username")).ascending())); //w widoku Zadań wyświetli wszystkie osoby
 
         return modelAndView;
     }
 
     @GetMapping("/edit/{id}")
-        // TODO: @Secured("ROLE_PROJECT_EDIT")
+    @Secured("ROLE_MANAGE_PROJECT")
     ModelAndView edit(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("issues/create");
 
         Issue issue = issueRepository.findById(id).orElse(null);
+        modelAndView.addObject("issue", issueRepository.findAllByEnabled(true));
         modelAndView.addObject("issue", issue);
-        modelAndView.addObject("projects", projectRepository.findAll(Sort.by(Sort.Order.by("code")).ascending())); // w widoku Zadań wyświetli wszystkie projekty
+        modelAndView.addObject("projects", projectRepository.findAllByEnabled(true)); // w widoku Zadań wyświetli wszystkie projekty
         modelAndView.addObject("person", personRepository.findAll(Sort.by(Sort.Order.by("username")).ascending())); //w widoku Zadań wyświetli wszystkie osoby
 
         return modelAndView;
     }
 
-
-
     @PostMapping("/save")
-    String save(@ModelAttribute Issue issue, Principal principal) {
+    @Secured("ROLE_MANAGE_PROJECT")
+    ModelAndView save(@ModelAttribute @Valid Issue issue,
+                      BindingResult bindingResult, Principal principal) {
+
+        ModelAndView modelAndView = new ModelAndView("issues/create");
+
+        if (bindingResult.hasErrors()) {
+
+            modelAndView.addObject("issue", issue);
+            modelAndView.addObject("projects", projectRepository.findAllByEnabled(true)); // w widoku Zadań wyświetli wszystkie projekty
+            modelAndView.addObject("person", personRepository.findAll(Sort.by(Sort.Order.by("username")).ascending())); //w widoku Zadań wyświetli wszystkie osoby
+            return modelAndView;
+        }
+        // walidacja - jeśli issue o tym kodzie istnieje w polu code wpisz komunikat
+
+        if (issue.getId() == null)
+
+        if (isIssueAlreadyExists(issue)) {
+            bindingResult.rejectValue("code", "duplicate.code");
+            return modelAndView;
+        }
+
         issueService.save(issue, principal.getName());
-        return "redirect:/issues";
+
+        modelAndView.setViewName("redirect:/issues");
+
+        return modelAndView;
     }
 
+    //metoda sprawdzajaca czy Issue z danym kodem istnieje
+    private boolean isIssueAlreadyExists(Issue issue) {
+        Issue existingIssue = issueRepository.findByCode(issue.getCode());
+        return existingIssue != null;
+    }
+
+
     @GetMapping("/delete/{id}")
+    @Secured("ROLE_MANAGE_PROJECT")
     ModelAndView delete(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("issues/delete");
 
@@ -93,14 +128,26 @@ public class IssueController {
     }
 
     @PostMapping("/delete")
+    @Secured("ROLE_MANAGE_PROJECT")
+    String delete(@ModelAttribute Issue issue) {
+        issue.setEnabled(false);
+        issueRepository.save(issue);
+
+        return "redirect:/issues";
+
+    }
+
+
+    /* rzeczywiście usuwa zadanie z bazy danych
+    @PostMapping("/delete")
     String delete(@ModelAttribute Issue issue) {
 
         issueRepository.deleteById(issue.getId());
 
         return "redirect:/issues";
 
-
     }
+    */
 
 
 }
