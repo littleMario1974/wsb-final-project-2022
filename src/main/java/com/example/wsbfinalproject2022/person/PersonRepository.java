d44671265d6a2aa71193b215dabb74da805c54f4
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-06-18 17:22:04 +0200

    dodanie adnotacji @Repository

--- src/main/java/com/example/wsbfinalproject2022/person/PersonRepository.java
+++ src/main/java/com/example/wsbfinalproject2022/person/PersonRepository.java
@@ -1,11 +1,13 @@
 package com.example.wsbfinalproject2022.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findAllByEnabled(Boolean enabled);
@@ -17,4 +19,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     Iterable<Person> findAllByUsernameOrPassword(String s1, String s2);
 
     Iterable<Person> findAllByDateCreatedAfter(Date d);
+
 }
