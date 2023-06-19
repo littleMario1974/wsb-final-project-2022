ec18fc6dfc3f64e61ca1885bac21a9d895768dfa
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-06-19 22:13:32 +0200

    zmiana tesu jednostkowego czy osoba o takim imieniu i mailu istnieje

--- src/test/java/com/example/wsbfinalproject2022/person/PersonServiceTest.java
+++ src/test/java/com/example/wsbfinalproject2022/person/PersonServiceTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -20,17 +18,14 @@ class PersonServiceTest {
     @Mock
     PersonRepository personRepository;
 
-    @Mock
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
 
     @InjectMocks
     PersonService personService;
 
     @Test
-    void personShouldExistAndHaveNoPassword() {
+    void personShouldExist() {
 //given
-        when(personRepository.findById(any())).thenReturn(Optional.of(Person.builder().id(1l).username("waclaw").password("hg8iuov5t3n4875t34gvibv5ntei4t63w86nf53tni87o23").build()));
+        when(personRepository.findById(any())).thenReturn(Optional.of(Person.builder().id(1l).username("waclaw").email("email@gmail.com").build()));
 
 
 //when
@@ -39,7 +34,7 @@ class PersonServiceTest {
 
 //then
         assertEquals("waclaw", testObj.get().getUsername());
-        assertNull(testObj.get().getPassword());
+        assertEquals("email@gmial.com", testObj.get().getEmail());
 
     }
 }
